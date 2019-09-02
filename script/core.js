7b3f81a231d5bbedf2152e7a3f331b2ccfe50461
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-09-02 11:46:28 -0300

    agrupando melhor funções auxiliares

--- script/core.js
+++ script/core.js
@@ -204,33 +204,6 @@ function calculaIdade(nascimento){
     }
 }
 
-
-function isEquivalent(a, b) {
-    // Create arrays of property names
-    var aProps = Object.getOwnPropertyNames(a);
-    var bProps = Object.getOwnPropertyNames(b);
-
-    // If number of properties is different,
-    // objects are not equivalent
-    if (aProps.length != bProps.length) {
-        return false;
-    }
-
-    for (var i = 0; i < aProps.length; i++) {
-        var propName = aProps[i];
-
-        // If values of same property are not equal,
-        // objects are not equivalent
-        if (a[propName] !== b[propName]) {
-            return false;
-        }
-    }
-
-    // If we made it this far, objects
-    // are considered equivalent
-    return true;
-}
-
 function getChecked(lista){
     for (var i=0; i < lista.length; i++){
         if (lista[i].checked){
@@ -257,6 +230,9 @@ function converteObjetoToJson(value){
     return JSON.stringify(value);
 }
 
+
+//Funções auxiliares externas
+
 function insertion_Sort(arr){
   for (var i = 1; i < arr.length; i++) 
   {
@@ -281,4 +257,30 @@ function insertion_Sort(arr){
     }
   }
   return arr;
+}
+
+function isEquivalent(a, b) {
+    // Create arrays of property names
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal,
+        // objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
 }
